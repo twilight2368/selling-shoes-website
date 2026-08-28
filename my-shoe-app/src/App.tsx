7cff54d5fa21750386d8bba50e522c756590e5ca
Anonymous <103519498+twilight2368@users.noreply.github.com>
2026-08-28 09:02:52 +0700

    init

---
+++ my-shoe-app/src/App.tsx
@@ -0,0 +1,12 @@
+//import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-blue-500 font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
